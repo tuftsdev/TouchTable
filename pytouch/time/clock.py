b7e878196dc305de0254ec501f4768fd6328204a
Andrew Li <andrew.li@tufts.edu>
2013-12-18 18:56:54 -0500

    updated clock with pygame func wrappers

--- pytouch/time/clock.py
+++ pytouch/time/clock.py
@@ -6,3 +6,18 @@ from pygame.time import *
 class Clock():
     def __init__(self, clock):
         self.clock = clock
+
+    def tick(framerate):
+        self.clock.tick(framerate)
+
+    def tick_busy_loop(framerate):
+        self.clock.tick_busy_loop(framerate)
+
+    def get_time():
+        return self.clock.get_time()
+
+    def get_rawtime():
+        return self.clock.get_rawtime()
+
+    def get_fps():
+        return self.clock.get_fps()
