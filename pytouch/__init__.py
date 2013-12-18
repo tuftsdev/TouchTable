11b8a83d978bbed8a0f342fe32420e3bb4e6ff3c
Andrew Li <andrew.li@tufts.edu>
2013-12-18 18:40:42 -0500

    updated pytouch init with a clock to test

--- pytouch/__init__.py
+++ pytouch/__init__.py
@@ -1,1 +1,23 @@
+
+__all__ = (
+    'init',
+    'Clock')
+
 __version__ = '0.0.1'
+
+import sys
+from pytouch.time import Clock
+
+clock = None
+
+def init():
+    pygame.init()
+    clock = pytouch.time.Clock(pygame.time.Clock())
+
+def Clock():
+    if clock = None:
+        # TODO: Convert to Logger, remove exit
+        # ERROR NOT INITIALIZED
+        sys.exit('ERROR: Pytouch not initialized')
+    else:
+        return clock
---
+++ pytouch/time/clock.py
@@ -0,0 +1,8 @@
+
+__all__ = ('Clock')
+
+from pygame.time import *
+
+class Clock():
+    def __init__(self, clock):
+        self.clock = clock
