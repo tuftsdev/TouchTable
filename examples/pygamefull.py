dd59a6516c3178ea34299295a24b28661c2e81f7
Andrew Li <andrew.li@tufts.edu>
2014-02-19 17:16:27 -0500

    fullscreen example

--- examples/pygamefull.py
+++ examples/pygamefull.py
@@ -21,4 +21,13 @@ if __name__ == '__main__':
                     sys.exit()
                 elif event.key == pygame.K_f:
                     screen = pygame.display.set_mode((800,600),pygame.FULLSCREEN)
-
+                    background = pygame.Surface(screen.get_size())
+                    background = background.convert()
+                    background.fill((250,250,250))
+                    screen.blit(background,(0,0))
+                elif event.key == pygame.K_g:
+                    screen = pygame.display.set_mode((800,600), 0, 32)
+                    background = pygame.Surface(screen.get_size())
+                    background = background.convert()
+                    background.fill((250,250,250))
+                    screen.blit(background,(0,0))
