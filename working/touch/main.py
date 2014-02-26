b7aa59f92237b8e080666fa3f73d1c0b54e8104b
Andrew Li <andrew.li@tufts.edu>
2014-02-25 19:42:55 -0500

    Pytuio integration

--- working/touch/main.py
+++ working/touch/main.py
@@ -1,1 +1,20 @@
-import tracking
+import pytuio, pygame
+
+
+
+
+
+
+
+# Testing Framework
+if __name__ == '__main__':
+    tracking = pytuio.Tracking()
+    print "loaded profiles:", tracking.profiles.keys()
+    print "list functions to access tracked objects:", tracking.get_helpers()
+    try:
+        while 1:
+            tracking.update()
+            for obj in tracking.cursors():
+                print obj.mot_accel
+    except KeyboardInterrupt:
+        tracking.stop()
