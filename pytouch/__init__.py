63eae3fab39090cd872ed0b2a13d6dfe99b24cf8
Aaron Wishnick <aaronwishnick@gmail.com>
2014-02-26 13:47:11 -0500

    moved old pytouch directory to pytouch_other, changed working to pytouch, moved whack a mole code into its own file, changed main.py to __init__.py of pytouch

--- pytouch/__init__.py
+++ pytouch/__init__.py
@@ -1,11 +1,29 @@
-# TO EDIT
-__all__ = []
+import pygame, touch, rectangle
+from pygame.locals import *
 
-__version__ = '0.0.1'
 
-import pygame
-
-def init():
+class PyTouch(object):
+  def __init__(self, bgcolor=(0,0,0)):
     pygame.init()
+    self.screen = pygame.display.set_mode((800,600))
+    self.touchTracker = touch.TouchTracker(800,600)
+    self.objects = []
+    self.bgcolor = bgcolor
+    self.clear()
 
-    
+  def update(self):
+    t = self.touchTracker.update()
+    if t != None:
+      for obj in self.objects:
+        obj.touchUpInside(t)
+    pygame.display.flip()
+
+  def drawRect(self, x, y, width, height, color='white', edge_thickness=0, surface=None):
+    if surface == None:
+      surface = self.screen
+    newRect = rectangle.Rectangle(surface,x,y,width,height,color,edge_thickness)
+    self.objects.append(newRect)
+    return newRect
+
+  def clear(self):
+    self.screen.fill(self.bgcolor)
