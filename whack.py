af47d474bab44d641f194f071d4298f8824bd35b
Aaron Wishnick <aaronwishnick@gmail.com>
2014-03-05 09:01:21 -0500

    update whack to new pytouch redrawing

--- whack.py
+++ whack.py
@@ -15,18 +15,15 @@ if __name__ == "__main__":
   rect = pytouch.drawRect(x*150+150+10, y*150+150+10,80,80, 'blue')
   rect.touchUpInsideHandler = touch_handler
 
-  timer_max = random.randint(50,100)
+  timer_max = random.randint(25,40)
   while 1:
     if timer > timer_max:
-      timer_max = random.randint(50,100)
-      pytouch.clear()
-      for i in range(0, 3):
-        for j in range(0, 3):
-          pytouch.drawRect(j*150+150, i*150+150, 100,100)
+      timer_max = random.randint(25,40)
       timer = 0
       x = random.randint(0,2)
       y = random.randint(0,2)
-      rect = pytouch.drawRect(x*150+150+10, y*150+150+10,80,80, 'blue')
-      rect.touchUpInsideHandler = touch_handler
+      rect.move(x*150+160, y*150+160)
+    elif timer == 1:
+      rect.changeColor('blue')
     timer += 1
     pytouch.update()
