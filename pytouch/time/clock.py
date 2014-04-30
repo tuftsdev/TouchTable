c3687dea95cad559678d821fe2a507cf9e97bcc3
Andrew Li <andrew.li@tufts.edu>
2014-04-30 16:09:45 -0400

    updated parameters to include self reference

--- pytouch/time/clock.py
+++ pytouch/time/clock.py
@@ -4,17 +4,17 @@ class Clock():
     def __init__(self):
         self.clock = pygame.time.Clock()
 
-    def tick(framerate):
+    def tick(self, framerate):
         self.clock.tick(framerate)
 
-    def tick_busy_loop(framerate):
+    def tick_busy_loop(self, framerate):
         self.clock.tick_busy_loop(framerate)
 
-    def get_time():
+    def get_time(self):
         return self.clock.get_time()
 
-    def get_rawtime():
+    def get_rawtime(self):
         return self.clock.get_rawtime()
 
-    def get_fps():
+    def get_fps(self):
         return self.clock.get_fps()
